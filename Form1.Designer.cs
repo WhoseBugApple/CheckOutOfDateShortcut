41e46754e2892cf9067b971299a2f0da369e5631
WhoseBugApple <M792715299@outlook.com>
2023-03-03 19:47:28 +0800

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace CheckOutOfDateShortcut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelInput = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanelButton = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.tableLayoutPanelOutput = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanelLog = new System.Windows.Forms.TableLayoutPanel();
+            this.labelLog = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.tableLayoutPanelInput.SuspendLayout();
+            this.tableLayoutPanelButton.SuspendLayout();
+            this.tableLayoutPanelOutput.SuspendLayout();
+            this.tableLayoutPanelLog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanelMain, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 1;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelInput, 0, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelButton, 0, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelOutput, 0, 2);
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelLog, 0, 3);
+            this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(83, 3);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 4;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(634, 444);
+            this.tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // tableLayoutPanelInput
+            // 
+            this.tableLayoutPanelInput.ColumnCount = 2;
+            this.tableLayoutPanelInput.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanelInput.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelInput.Controls.Add(this.textBoxInput, 1, 0);
+            this.tableLayoutPanelInput.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanelInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelInput.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanelInput.Name = "tableLayoutPanelInput";
+            this.tableLayoutPanelInput.RowCount = 1;
+            this.tableLayoutPanelInput.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelInput.Size = new System.Drawing.Size(628, 36);
+            this.tableLayoutPanelInput.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInput.Location = new System.Drawing.Point(88, 5);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(537, 25);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanelButton
+            // 
+            this.tableLayoutPanelButton.ColumnCount = 1;
+            this.tableLayoutPanelButton.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelButton.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelButton.Controls.Add(this.buttonRun, 0, 0);
+            this.tableLayoutPanelButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelButton.Location = new System.Drawing.Point(3, 45);
+            this.tableLayoutPanelButton.Name = "tableLayoutPanelButton";
+            this.tableLayoutPanelButton.RowCount = 1;
+            this.tableLayoutPanelButton.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelButton.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanelButton.Size = new System.Drawing.Size(628, 36);
+            this.tableLayoutPanelButton.TabIndex = 1;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRun.Location = new System.Drawing.Point(3, 3);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(622, 30);
+            this.buttonRun.TabIndex = 0;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // tableLayoutPanelOutput
+            // 
+            this.tableLayoutPanelOutput.ColumnCount = 1;
+            this.tableLayoutPanelOutput.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelOutput.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanelOutput.Controls.Add(this.listView1, 0, 1);
+            this.tableLayoutPanelOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelOutput.Location = new System.Drawing.Point(3, 87);
+            this.tableLayoutPanelOutput.Name = "tableLayoutPanelOutput";
+            this.tableLayoutPanelOutput.RowCount = 2;
+            this.tableLayoutPanelOutput.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanelOutput.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelOutput.Size = new System.Drawing.Size(628, 330);
+            this.tableLayoutPanelOutput.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(622, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Links that Missing Target";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 18);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(622, 309);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File Name";
+            this.columnHeader1.Width = 242;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Target";
+            this.columnHeader2.Width = 306;
+            // 
+            // tableLayoutPanelLog
+            // 
+            this.tableLayoutPanelLog.ColumnCount = 1;
+            this.tableLayoutPanelLog.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelLog.Controls.Add(this.labelLog, 0, 0);
+            this.tableLayoutPanelLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelLog.Location = new System.Drawing.Point(3, 423);
+            this.tableLayoutPanelLog.Name = "tableLayoutPanelLog";
+            this.tableLayoutPanelLog.RowCount = 1;
+            this.tableLayoutPanelLog.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanelLog.Size = new System.Drawing.Size(628, 18);
+            this.tableLayoutPanelLog.TabIndex = 3;
+            // 
+            // labelLog
+            // 
+            this.labelLog.AutoSize = true;
+            this.labelLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelLog.Location = new System.Drawing.Point(3, 0);
+            this.labelLog.MaximumSize = new System.Drawing.Size(0, 24);
+            this.labelLog.MinimumSize = new System.Drawing.Size(0, 10);
+            this.labelLog.Name = "labelLog";
+            this.labelLog.Size = new System.Drawing.Size(622, 18);
+            this.labelLog.TabIndex = 0;
+            this.labelLog.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "CheckOutOfDateShortCut";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.tableLayoutPanelInput.ResumeLayout(false);
+            this.tableLayoutPanelInput.PerformLayout();
+            this.tableLayoutPanelButton.ResumeLayout(false);
+            this.tableLayoutPanelOutput.ResumeLayout(false);
+            this.tableLayoutPanelOutput.PerformLayout();
+            this.tableLayoutPanelLog.ResumeLayout(false);
+            this.tableLayoutPanelLog.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelInput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelButton;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelLog;
+        private System.Windows.Forms.Label labelLog;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Shell32;
+using System.Security.AccessControl;
+
+namespace CheckOutOfDateShortcut
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            utils.disablePublishingTask(this);
+            utils.log(this.labelLog, "Running");
+            String directory = textBoxInput.Text;
+            if (!utils.isValidDirectory(directory))
+            {
+                utils.log(this.labelLog, "Invalid Directory");
+                utils.enablePublishingTask(this);
+                return;
+            }
+
+            new System.Threading.Thread(() =>
+            {
+                // find lnk files
+                string[] lnkFilePaths = Directory.GetFiles(directory, "*.lnk");
+
+                // filter lnks that missing target
+                int failedCount = 0;
+                IEnumerable<string> filtered = System.Linq.Enumerable.Where(
+                    lnkFilePaths,
+                    lnk => {
+                        try  // read target and figure out is target exist
+                        {
+                            // Console.WriteLine(lnk);
+                            String target = utils.getLnkTarget(lnk);
+                            // Console.WriteLine("    " + target);
+                            String dirOnly = System.IO.Path.GetDirectoryName(target);
+                            // b -> is can't find target
+                            bool b = Path.IsPathRooted(target) &&
+                                !target.StartsWith("::{") &&
+                                (!utils.ExistFileOrDirectory(target));
+                            return b;
+                        }
+                        catch (Exception e1)
+                        {
+                            failedCount++;
+                            Console.WriteLine("Faile to read Target for " + lnk);
+                            return false;
+                        }
+                    });
+                string[] lnks = filtered.ToArray();
+                string[] targets = new string[lnks.Length];
+                for(int i=0; i<targets.Length; i++)
+                {
+                    targets[i] = utils.getLnkTarget(lnks[i]);
+                }
+
+                // notify winform thread to show result
+                this.BeginInvoke(new MyDelegate(() =>
+                {
+                    if (utils.showResult(listView1, lnks, targets))
+                    {
+                        utils.log(this.labelLog,
+                            lnks.Length + " lnks Missing Target, " +
+                            lnkFilePaths.Length + " lnks Checked, " + 
+                            failedCount + " lnks Failed to be Checked");
+                    } else
+                    {
+                        utils.log(this.labelLog, "Failed to Show Result");
+                    }
+                    utils.enablePublishingTask(this);
+                }));
+            }).Start();
+        }
+        delegate void MyDelegate();
+
+        private Utils utils = new Utils();
+        private class Utils
+        {
+            public void log(Label label, string text)
+            {
+                label.Text = text;
+            }
+
+            public bool showResult(ListView listView, string[] lnkFilePaths, string[] lnkFileTargets)
+            {
+                listView.Items.Clear();
+                if (lnkFilePaths.Length != lnkFileTargets.Length)
+                    return false;
+                for (int i=0; i<lnkFilePaths.Length; i++)
+                {
+                    string lnk = lnkFilePaths[i];
+                    string target = lnkFileTargets[i];
+                    listView.Items.Add(
+                        new ListViewItem(
+                            new string[]{ Path.GetFileName(lnk), target}));
+                }
+                return true;
+            }
+
+            public bool isValidDirectory(String path)
+            {
+                if (path == null) return false;
+                if (!Directory.Exists(path)) return false;
+                return true;
+            }
+            
+            public bool ExistFileOrDirectory(String path)
+            {
+                return File.Exists(path) || Directory.Exists(path);
+            }
+
+            public void disablePublishingTask(Form1 form)
+            {
+                form.textBoxInput.Enabled = false;
+                form.buttonRun.Enabled = false;
+            }
+
+            public void enablePublishingTask(Form1 form)
+            {
+                form.textBoxInput.Enabled = true;
+                form.buttonRun.Enabled = true;
+            }
+
+            private GetLnkTarget myGetLnkTarget = new GetLnkTargetByResolveIt();
+            public String getLnkTarget(String lnk)
+            {
+                return myGetLnkTarget.getLnkTarget(lnk);
+            }
+        }
+    }
+
+    interface GetLnkTarget
+    {
+        String getLnkTarget(String shortcutPath);
+    }
+
+    // according to https://github.com/libyal/liblnk/blob/main/documentation/Windows%20Shortcut%20File%20(LNK)%20format.asciidoc
+    class GetLnkTargetByResolveIt : GetLnkTarget
+    {
+        String GetLnkTarget.getLnkTarget(String shortcutPath)
+        {
+            FileStream stream = null;
+            BinaryReader reader = null;
+            try
+            {
+                stream = new FileStream(shortcutPath, FileMode.Open);
+                reader = new BinaryReader(stream);
+
+                // read file header
+                {
+                    int size = 76;  // n byte
+                    reader.ReadBytes(size);
+                }
+
+                // read link target indentifier
+                {
+                    // int sizeOfRemain = reader.ReadInt16();
+                    int size = reader.ReadInt16() + 2;
+                    reader.BaseStream.Position -= 2;
+                    reader.ReadBytes(size);
+                }
+
+                // read location information
+                {
+                    // start index of location information
+                    long startIdxOfLocationInformation =
+                        reader.BaseStream.Position;
+                    int locationInformationSize =
+                        reader.ReadInt32();
+                    int locationInformationHeaderSize =
+                        reader.ReadInt32();
+                    reader.ReadInt32();
+                    reader.ReadInt32();
+                    // offset from (start of location information)
+                    int offsetToLocalPath = reader.ReadInt32();
+
+                    // start index of local path
+                    long startIdxOfLocalPath =
+                        startIdxOfLocationInformation + offsetToLocalPath;
+
+                    // read until local path
+                    while (reader.BaseStream.Position < startIdxOfLocalPath)
+                    {
+                        reader.ReadByte();
+                    }
+                    // read local path
+                    {
+                        // reader until \0
+                        LinkedList<byte> bList = new LinkedList<byte>();
+                        while (reader.BaseStream.Position != reader.BaseStream.Length)
+                        {
+                            byte bInner = reader.ReadByte();
+                            if (bInner == 0) break;
+                            bList.AddLast(bInner);
+                        }
+                        // return
+                        return Encoding.Default.GetString(bList.ToArray());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            finally
+            {
+                stream?.Close();  // close if not null
+                reader?.Close();
+            }
+        }
+    }
+
+    // by calling shell32.dll
+    class GetLnkTargetByShell : GetLnkTarget
+    {
+        Shell shell = new Shell();
+        String GetLnkTarget.getLnkTarget(String shortcutPath)
+        {
+            string pathOnly = System.IO.Path.GetDirectoryName(shortcutPath);
+            string filenameOnly = System.IO.Path.GetFileName(shortcutPath);
+
+            Folder folder = shell.NameSpace(pathOnly);
+            FolderItem folderItem = folder.ParseName(filenameOnly);
+            if (folderItem != null)
+            {
+                Shell32.ShellLinkObject link = (Shell32.ShellLinkObject)folderItem.GetLink;
+                return link.Target.Path;
+            }
+            return string.Empty;
+        }
+    }
+}
